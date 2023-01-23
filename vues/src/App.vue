3536bd38bfb2761643f92f04ad43ff0c16c71f17
Maishmaina <simpledaniel.1818@gmail.com>
2023-01-23 10:09:27 +0300

    git cli intro

---
+++ vues/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="app">
+  <h2>{{title}}</h2>
+</div>
+</template>
+<script>
+export default {
+  name: 'App',
+
+  data(){
+    return {
+      title:'Title vue app running'
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
